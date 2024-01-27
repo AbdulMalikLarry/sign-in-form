c6540098ac03a4bed4d05b1b0176bb5818cd6eb9
AbdulMalikLarry <abdulmaliklarry@gmail.com>
2024-01-26 18:47:59 -0800

    initialize git

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Form } from './components/form'
+import {Header} from './components/Header'
+
+function App() {
+
+
+  return (
+    
+    <div className='form'>
+
+        <div className='form2'>
+
+        <Header/>
+        <Form/>
+
+
+
+        </div>
+
+
+
+    </div>
+    
+  
+  
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import '../styles/Header.css'
+
+export const Header = () => {
+    return (
+
+            <div>
+                <h1 className='first-header-text' > YourCompany</h1>
+                
+                <div className='second-header-box'>
+                    <h2 className='second-header-box-first-text'>Sign Up for Our Newsletter</h2>
+                    <h3 className='second-header-box-second-text '>Fill out your information below to sign up for our informative newsletter</h3>
+                </div>
+                
+            </div>
+
+    )
+
+}
+
